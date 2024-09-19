8fd33a844a593a6f94de5d077ba109c6cffc2dd1
vaibhavmakwana45 <vaibhav_makwana@sparrowsofttech.com>
2024-09-19 10:45:39 +0530

    first-commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
 const { generateAndSavePdf } = require("./generatePdf");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
@@ -15,6 +17,23 @@ app.post("/convert", async (req, res) => {
     return res.status(200).json({
       filename,
     });
+  } catch (error) {
+    return res.status(500).json({ error, message: error.message });
+  }
+});
+
+app.get("/get/:filename", async (req, res) => {
+  try {
+    const { filename } = req.params;
+    const filePath = path.join(__dirname, "files", filename);
+    if (!fs.existsSync(filePath)) {
+      res.statusCode = 404;
+      res.end(`File not found: ${filename}`);
+      return;
+    }
+
+    const fileStream = fs.createReadStream(filePath);
+    fileStream.pipe(res);
   } catch (error) {
     return res.status(500).json({ error, message: error.message });
   }
